e6062e0932da231112b1fd00bc4612b6f532edfc
PascalRode <pascal.rode@gmail.com>
2014-12-17 23:47:52 +0100

    Commit me

--- guinet_demo/SAPSystemConnect.vb
+++ guinet_demo/SAPSystemConnect.vb
@@ -14,50 +14,6 @@ Class SAPSystemConnect
 
     Public Event ConfigurationChanged As RfcDestinationManager.ConfigurationChangeHandler
 
-    Public Function GetParameters_obsolete(ByVal destinationName As String) As RfcConfigParameters
-        Dim parms As New RfcConfigParameters()
-
-        Dim choose = 1
-
-        Select Case choose
-
-            Case 1
-                If "Dev".Equals(destinationName) Then
-
-                    parms.Add(RfcConfigParameters.AppServerHost, "85.214.113.141")
-                    parms.Add(RfcConfigParameters.Name, "ECC")
-                    parms.Add(RfcConfigParameters.SystemNumber, "00")
-                    parms.Add(RfcConfigParameters.User, "s10connect")
-                    parms.Add(RfcConfigParameters.Password, "connect")
-                    parms.Add(RfcConfigParameters.Client, "800")
-                    parms.Add(RfcConfigParameters.Language, "DE")
-                    parms.Add(RfcConfigParameters.PoolSize, "5")
-                    parms.Add(RfcConfigParameters.PeakConnectionsLimit, "10")
-
-                    parms.Add(RfcConfigParameters.ConnectionIdleTimeout, "600")
-                End If
-
-            Case 2
-                If "Dev".Equals(destinationName) Then
-
-                    parms.Add(RfcConfigParameters.AppServerHost, "/H/localhost/S/3200")
-                    parms.Add(RfcConfigParameters.SystemNumber, "00")
-                    parms.Add(RfcConfigParameters.Name, "ECC")
-                    parms.Add(RfcConfigParameters.User, "")
-                    parms.Add(RfcConfigParameters.Password, "")
-                    parms.Add(RfcConfigParameters.Client, "800")
-                    parms.Add(RfcConfigParameters.Language, "DE")
-                    parms.Add(RfcConfigParameters.PoolSize, "5")
-                    parms.Add(RfcConfigParameters.PeakConnectionsLimit, "10")
-
-                    parms.Add(RfcConfigParameters.ConnectionIdleTimeout, "600")
-                End If
-
-        End Select
-
-        Return parms
-    End Function
-
     Public Function ChangeEventsSupported1() As Boolean Implements SAP.Middleware.Connector.IDestinationConfiguration.ChangeEventsSupported
 
     End Function
